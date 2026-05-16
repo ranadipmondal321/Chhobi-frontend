b0a43b624f99e628354d21c2dd1049f09cb54cab
ranadipmondal321 <ranadipmondal321@gmail.com>
2026-05-16 17:14:18 +0530

    Update authStore.js

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 axios.defaults.withCredentials = true;
 
-const API_URL = "http://localhost:5000/api"
+const API_URL = "https://chhobi-2.onrender.com"
 
 export const useAuthStore = create((set) => ({
     // initial states
